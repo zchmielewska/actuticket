0e901d5f7039b16806dffbb3c7f5a3af7c0c6397
Chmielewska <Zuzanna.Chmielewska@aegon.nl>
2022-04-21 08:58:42 +0000

    read allowed hosts from env

--- actuticket/settings.py
+++ actuticket/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9c9t0udgl_lfk8y+)fi)$us-mfe-n@r8nex^^4bjr%5w8_d@$l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
